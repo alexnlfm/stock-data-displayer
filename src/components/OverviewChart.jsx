001e3b1e8476218a147c985e41e1e91a3eed72e2
alexander_shvartskopf <alex.s@progg.io>
2024-05-18 19:03:05 +0300

    Finalizing OverviewChart component implementation

--- src/components/OverviewChart.jsx
+++ src/components/OverviewChart.jsx
@@ -1,7 +1,59 @@
+import { useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import styled from 'styled-components';
+
+import useChartData from "../customHooks/useChartData";
+import { colors, BORDER_RADIUS } from '../constants/styles';
+
+const periods = [
+  { label: '1 day', amount: 1, units: 'days' },
+  { label: '1 week', amount: 1, units: 'weeks' },
+  { label: '1 month', amount: 1, units: 'months' },
+  { label: '1 quater', amount: 1, units: 'quarters' },
+  { label: '1 year', amount: 1, units: 'years' }
+];
+
 function OverviewChart() {
+  const [selectedPeriod, setSelectedPeriod] = useState({ amount: 1, units: 'days' });
+  const { chartData } = useChartData(selectedPeriod);
+  
   return (
-    <div>Overview Chart</div>
+    <>
+      <PeriodsButtons>
+        {periods.map(({ label, amount, units }) => (
+          <PeriodButton
+            key={`${amount}-${units}`}
+            isSelected={amount === selectedPeriod.amount && units === selectedPeriod.units}
+            onClick={() => setSelectedPeriod({ amount, units })}
+          >
+            {label}
+          </PeriodButton>
+        ))}
+      </PeriodsButtons>
+      <LineChart width={1800} height={300} data={chartData}>
+        <Line type="monotone" dataKey="Volume" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="Date" />
+        <YAxis />
+      </LineChart>
+    </>
   );
 }
+
+const PeriodsButtons = styled.div`
+  width: 30%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto 20px auto;
+`;
+
+const PeriodButton = styled.div`  
+  border-radius: ${BORDER_RADIUS}px;
+  cursor: pointer;
+  padding: 5px 10px;
+  border: ${({ isSelected }) => isSelected ? 'none' : `1px solid ${colors.GREY2}`};
+  background-color: ${({ isSelected }) => isSelected ? colors.BLACK : 'transparent'};
+  color: ${({ isSelected }) => isSelected ? colors.BRAND_GREEN : 'inherit'};
+`;
 
 export default OverviewChart;
---
+++ src/customHooks/useChartData.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+
+const URL_PREFIX = 'https://test.fxempire.com/api/v1/en/stocks/chart/candles?Identifier=AAPL.XNAS&IdentifierType=Symbol&AdjustmentMethod=All&IncludeExtended=False&period=30&Precision=Minutes&';
+const URL_SUFFIX = '%2023:59&_fields=ChartBars.StartDate,ChartBars.High,ChartBars.Low,ChartBars.StartTime,ChartBars.Open,ChartBars.Close,ChartBars.Volume';
+
+const datesFormat = 'MM/DD/YYYY';
+
+function useChartData(selectedPeriod) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    async function asyncFunc() {      
+      setIsLoading(true);
+      const startDate = moment().subtract(selectedPeriod.amount, selectedPeriod.units).format(datesFormat);
+      const endDate = moment().format(datesFormat);
+      const url = `${URL_PREFIX}StartTime=${startDate}&EndTime=${endDate}${URL_SUFFIX}`;
+      const result = await fetch(url);
+      const data = await result.json();
+      setChartData(data);
+      setIsLoading(false);
+    }
+    asyncFunc();
+  }, [selectedPeriod]);
+
+  return { isLoading, chartData };
+}
+
+export default useChartData;
